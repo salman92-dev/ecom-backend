a5d0282b049cb9e988b6607ad3438636216c4e86
salman92-dev <124353689+salman92-dev@users.noreply.github.com>
2026-02-03 15:14:24 +0500

    Update database connection string environment variable

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -2,7 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.Next_PUBLIC_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 5,              // keep low for serverless
   idleTimeoutMillis: 30000,
@@ -10,3 +10,4 @@ const pool = new Pool({
 });
 
 export default pool;
+
